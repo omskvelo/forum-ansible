9a412521ecf4d12200dd9d3eabeb61ffa1a5e418
Ivan Zezyulya <ivan@zoid.cc>
2019-01-09 22:25:17 +0600

    copy constants.php as well

---
+++ roles/omskvelo-ipb-config/templates/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+
+define( 'STORE_METHOD', 'Database' );
+define( 'STORE_CONFIG', '[]' );
+define( 'CACHE_METHOD', 'Memcache' );
+define( 'CACHE_CONFIG', '{"servers":["127.0.0.1:11211"]}' );
+define( 'CACHE_PAGE_TIMEOUT', 30 );
+define( 'SUITE_UNIQUE_KEY', '2e655b9932' );
+
+
+?>
