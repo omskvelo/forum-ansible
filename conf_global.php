41c502b7938f95800779228232c76cff9ee0c10c
Ivan Zezyulya <ivan@zoid.cc>
2019-01-09 19:34:09 +0600

    changes

---
+++ conf_global.php
@@ -0,0 +1,14 @@
+$INFO = [
+	"sql_host"          => "127.0.0.1",
+	"sql_database"      => "{{omskvelo_sql_db}}",
+	"sql_user"          => "{{omskvelo_sql_user}}",
+	"sql_pass"          => "{{omskvelo_sql_password}}",
+	"sql_tbl_prefix"    => "ibf_",
+	"sql_utf8mb4"       => true,
+	"board_start"       => "1266286629",
+	"installed"         => "1",
+	"base_url"          => "https://omskvelo.ru/",
+	"guest_group"       => "2",
+	"member_group"      => "3",
+	"admin_group"       => "4",
+];
