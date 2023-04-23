c7c2a7644129ccd68521e1281cf1ca5d88f55410
Ivan Zezyulya <ivan@zoid.cc>
2023-04-24 00:23:29 +0600

    Switch to MariaDB and Redis

--- roles/omskvelo-ipb-config/templates/constants.php
+++ roles/omskvelo-ipb-config/templates/constants.php
@@ -1,12 +1,11 @@
 <?php
+define( 'REPORT_EXCEPTIONS', true );
 
-
+define( 'REDIS_ENABLED', true );
 define( 'STORE_METHOD', 'Database' );
 define( 'STORE_CONFIG', '[]' );
-define( 'CACHE_METHOD', 'Memcache' );
-define( 'CACHE_CONFIG', '{"servers":["127.0.0.1:11211"]}' );
+define( 'CACHE_METHOD', 'Redis' );
+define( 'CACHE_CONFIG', '{"server":"127.0.0.1","port":6379,"password":""}' );
 define( 'CACHE_PAGE_TIMEOUT', 30 );
 define( 'SUITE_UNIQUE_KEY', '2e655b9932' );
-
-
 ?>
